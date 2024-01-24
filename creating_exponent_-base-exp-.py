07c22a7c1e2fcf3cd5c67729930beb0a8521dc3f
veronbungag <rveron.bungag@gmail.com>
2024-01-24 23:41:09 +0800

    add variables

--- creating_exponent_-base-exp-.py
+++ creating_exponent_-base-exp-.py
@@ -1,6 +1,8 @@
 #define a function for exponent
 def exponent(base, exp):
-#add variable number and result
+    #add variable number and result
+    number = exp
+    result = 1
 #while loop for number is greater than 0
 #print result for the exponent
 #checking if the function is working
