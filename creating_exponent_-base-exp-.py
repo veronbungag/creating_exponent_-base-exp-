9016e5c83af44e9e60bc1a028d381369af35496c
veronbungag <rveron.bungag@gmail.com>
2024-01-24 23:51:01 +0800

    while loop done

--- creating_exponent_-base-exp-.py
+++ creating_exponent_-base-exp-.py
@@ -3,6 +3,9 @@ def exponent(base, exp):
     #add variable number and result
     number = exp
     result = 1
-#while loop for number is greater than 0
+    #while loop for number is greater than 0
+    while number > 0:
+        result = result * base
+        number = number - 1
 #print result for the exponent
 #checking if the function is working
